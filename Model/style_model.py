c90aff66ac9ad6fcc97ddbac788e02ed7580cc78
yujinni <86652449+yujin37@users.noreply.github.com>
2024-12-07 19:54:58 +0900

    refactor: stylemodel을 Lllama 모델로 분석하도록 변경

--- Model/style_model.py
+++ Model/style_model.py
@@ -1,28 +1,61 @@
-from transformers import T5Tokenizer, T5ForConditionalGeneration
-import torch
-import token_model
+from openai import OpenAI
+import json
+import firebase_admin
+from firebase_admin import credentials, firestore
 
-# T5 모델과 T5 토크나이저 로드
-model_name = "t5-small"  # 예시로 작은 T5 모델 사용, 필요에 따라 변경 가능
-tokenizer = T5Tokenizer.from_pretrained(model_name)
-model = T5ForConditionalGeneration.from_pretrained(model_name)
+#llama 모델 관련 설정
+with open('../config.json', 'r') as config_file:
+    config = json.load(config_file)
+    SECRET_KEY = config.get('LAMBDA_KEY')
 
-# 사용자 입력 텍스트 (예시)
+if SECRET_KEY is None:
+    raise ValueError("SECRET_KEY가 설정되지 않았습니다.")
+
+# Lambda Labs API 키와 베이스 URL 설정
+api_key = SECRET_KEY  # Lambda Labs API Key
+api_base_url = "https://api.lambdalabs.com/v1"
+model = "llama3.1-70b-instruct-berkeley"
+
+client = OpenAI(
+    api_key=api_key,
+    base_url=api_base_url,
+)
+
+#사용자 데이터 가져오기.
+cred = credentials.Certificate("../serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+# 사용자 입력 텍스트
 user_input = """
 백준 문제 풀이를 하다가 새로운 개념이 있어서 문제 풀이와 함께 정리해보려고 한다.
 문제 정보는 골드바흐의 추측: 2보다 큰 짝수는 두 소수의 합으로 나타낼 수 있다.
 """
-
-# 텍스트를 "extract keywords:"로 분석하기
-input_text = "extract keywords: " + user_input  # 'extract keywords:'라는 프롬프트 추가
-
-# 토크나이징
-encoded_inputs = token_model.tokenizer_model(input_text) 
-
-# 모델에 입력하고 출력 얻기
-with torch.no_grad():
-    output = model.generate(encoded_inputs['input_ids'], max_length=200)
-
-# 출력 텍스트 디코딩
-decoded_output = tokenizer.decode(output[0], skip_special_tokens=True)
-print(decoded_output)
+try:
+    docs = db.collection("blogdata").get()
+    for doc in docs:
+        user_input = doc.to_dict().values()
+        user_input = ''.join(map(str, user_input))
+        #print(user_input)
+        # Chat Completion 요청
+        
+        response = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "system",
+                    "content": (
+                        "You are a text analysis assistant. "
+                        "Analyze the user's input, describe the writing style. "
+                        "This text is markdown type"
+                        "Focus on tone, word choice, and structure. Say Simple"
+                    ),
+                },
+                {"role": "user", "content": user_input},
+            ],
+            model=model,
+        )
+except Exception as e:
+    print(f'오류 밠생: {e}')
+# 출력 결과
+print("Response:")
+print(response)
