e53ac24bd2b0cd9d0fcbae4d04b62a1aa8ddd39b
yujinni <86652449+yujin37@users.noreply.github.com>
2024-12-17 03:45:06 +0900

    chore: total_model 에서 각 버튼 상태별 호출 기능 및 실행 시 spinner 구현

--- pages/1_Home.py
+++ pages/1_Home.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import sidebar
+import requests
 
 def home():
     sidebar.sidebar_menu()
@@ -19,13 +20,47 @@ def home():
         hashtag_gen = st.checkbox('Recommend Hashtag')
 
         # 버튼이 눌렸는지 확인
-        if st.button('Generation'):
+        if st.button('Generation') and text:
             if blog_gen:
                 st.text("블로그 생성 기능 작동")
+                with st.spinner('블로그 생성 중...'):
+                    response_1 = requests.post(
+                    "http://127.0.0.1:8000/blog", json={"input_text": text, "user": text}
+
+                    )
+                    if response_1.status_code == 200:
+                        processed_text_1 = response_1.json()
+                        #processed_text = response.json()["processed_text"]
+                        st.write("처리된 텍스트:", processed_text_1)
+                    else:
+                        st.write("API 호출 실패")
             if summary_gen:
                 st.text('요약 기능 작동')
+                with st.spinner('요약 중...'):
+                    response_2 = requests.post(
+                    "http://127.0.0.1:8000/summary", json={"input_text": text}
+
+                    )
+                    if response_2.status_code == 200:
+                        processed_text_2 = response_1.json()
+                        #processed_text = response.json()["processed_text"]
+                        st.write("처리된 텍스트:", processed_text_2)
+                    else:
+                        st.write("API 호출 실패")
             if hashtag_gen:
                 st.text('해시태그 추천 기능 작동')
+                with st.spinner('해시태그 추천 중...'):
+                    response_3 = requests.post(
+                    "http://127.0.0.1:8000/hashtag", json={"input_text": text}
+
+                    )
+                    if response_3.status_code == 200:
+                        processed_text_3 = response_1.json()
+                        #processed_text = response.json()["processed_text"]
+                        st.write("처리된 텍스트:", processed_text_3)
+                    else:
+                        st.write("API 호출 실패")
+        
 
 if __name__ == "__main__":
     home()
